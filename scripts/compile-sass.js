cf4ba6c3eb1fbd432c96bb3982cd458d6645a036
Cameron Cuff <dev.ctcuff@gmail.com>
2019-06-13 19:45:55 -0400

    node-sass is now used to compile scss

--- scripts/compile-sass.js
+++ scripts/compile-sass.js
@@ -1,24 +1,34 @@
-const { exec } = require('child_process');
 const fs = require('fs');
-
-const stylesDir = 'src/styles';
-
-// Only compile the css files
+const sass = require('node-sass');
+const stylesDir = 'src/styles/';
 const files = fs.readdirSync(stylesDir).filter(file => file.endsWith('.scss'));
+
+console.log(`Found ${files.length} scss files`);
 
 files.forEach(infile => {
   // Strip the .scss extension and add .css
-  const outfile = infile
-    .split('.')
+  const outfile = infile.split('.')
     .slice(0, -1)
     .join('.')
     .concat('.css');
 
-  exec(`sass ${stylesDir}/${infile} ${stylesDir}/${outfile}`, err => {
-    if (err) {
-      console.log(err);
+  const options = {
+    file: stylesDir + infile,
+    outputStyle: 'expanded'
+  };
+
+  sass.render(options, (error, result) => {
+    if (error) {
+      console.log(`Couldn't compile ${outfile}`, error);
       return;
     }
-    console.log(`compiled ${infile} => ${outfile}`);
+
+    fs.writeFile(stylesDir + outfile, result.css, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`${infile} compiled to ${outfile}`);
+    });
   });
 });
