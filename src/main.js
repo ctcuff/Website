d618960f8c56eb775a1bf64fbb656213328e8cbe
ctcuff <dev.ctcuff@gmail.com>
2020-07-26 02:25:54 -0400

    Removed unnecessary dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
 import CSSPlugin from 'gsap/CSSPlugin'
-import '@/scss/global.scss'
+import './scss/global.scss'
 import 'splitting/dist/splitting.css'
 import 'splitting/dist/splitting-cells.css'
-import 'locomotive-scroll/dist/locomotive-scroll.css'
 
 // Needed to make sure animations work in production
 // see https://github.com/greensock/GreenSock-JS/issues/285#issuecomment-433729632
