5db630a8c9a1d92009a4007e57853892d1efc6d7
ctcuff <dev.ctcuff@gmail.com>
2020-07-28 19:36:38 -0400

    Fixed typos

--- src/project-info.js
+++ src/project-info.js
@@ -52,7 +52,7 @@ const projectData = [
           only 7 parking garages, you can imagine how difficult it is at times to find a
           parking space. Thankfully, we have a website that shows students how full each garage is.
           The problem: it doesn't show past days, and there's no public API for it. UCF Parking is a
-          website (and API) that allows anyone to see how full each of UCF's 7 garages were at the top 
+          website (and API) that allows anyone to see how full each of UCF's 7 garages were at the top
           of each hour. With over 13,000 data points, it's now easier that ever to determine where
           to park.`
       },
@@ -63,7 +63,7 @@ const projectData = [
           was on soon turned into a full fledged API! But how you might ask? Let's break
           down this project into smaller problems. I wanted a way to automatically scrape
           data at set intervals, save that data, query that data, and display that data. A
-          few things came to mind to solve these problems: Vue, Python, MongoDB, and Heroku.
+          few things came to mind to solve these problems: Python, MongoDB, Vue, and Heroku.
           Every hour, Heroku runs a command that tells a Python server to scrape UCF's
           website. The data it scrapes is saved as JSON into a MongoDB database. Vue comes
           along, asks that Python server for some data, and displays it with some fancy
@@ -79,8 +79,8 @@ const projectData = [
           efficiently. What if someone queries for only one garage, or maybe all 7? Lastly,
           and probably most importantly, deployment. Heroku is my all time favorite hosting
           platform. I learned about their monitoring system and was able to reduce my
-          project's memory usage and response time because of it. The even offer continuous
-          integration and deployment, so pushing the next version of my side is as easy as
+          project's memory usage and response time because of it. They even offer continuous
+          integration and deployment, so pushing the next version of my site is as easy as
           pushing to a branch on GitHub.`
       }
     ]
@@ -100,9 +100,9 @@ const projectData = [
         heading: 'Write code together',
         text: `
         Imagine this: you've got your coding interview scheduled. It's online. You're
-        excited because you finally get to showcase you coding skills. The time comes.
+        excited because you finally get to showcase your coding skills. The time comes.
         You get the link. You open up the website... and you're asked to write
-        code on Google doc... yikes... Wouldn't it be nice to write code in your browser,
+        code on Google Docs... yikes... Wouldn't it be nice to write code in your browser,
         like you do with VSCode? Well fear not! LiveCode is a website that lets you write
         code with other people. Just sign in, connect to a workspace, and code like you
         never even left your text editor.`
@@ -111,7 +111,7 @@ const projectData = [
         heading: 'How it works',
         text: `
           Sockets! After signing in, you'll be able to connect to another
-          users workspace or have others connect to yours. It works by broadcasting changes made
+          users' workspace or have others connect to yours. It works by broadcasting changes made
           to your online editor to other users connected to your workspace using a library called
           socket.io. A tiny NodeJS server handles socket events to ensure content is only broadcast
           to users in the proper room. Google's Firebase is used to determine when users are online,
@@ -124,11 +124,11 @@ const projectData = [
       {
         heading: 'What I learned',
         text: `
-          In JavaScript, there's a library for everything. One of the biggest things I 
+          In JavaScript, there's a library for everything. One of the biggest things I
           learned from this project was not to re-invent the wheel. There's a time when re-inventing
           existing solutions is a good idea (you learn stuff in the process), but implementing user
           authentication from scratch is not one of those times. Integrating Google's Firebase SDK,
-          I learned how to authenticate users with their Github and Google account, and I learned 
+          I learned how to authenticate users with their Github and Google account, and I learned
           how to implement Firebase security rules, preventing outside users from accessing
           information they shouldn't have access to. Because a large part of the project was front
           end, I learned a ton about material components, color, and responsive design. Yes,
@@ -152,25 +152,25 @@ const projectData = [
         text: `
           If you've ever used JavaScript, odds are, at some point, you've typed:
           npm install <package>. One day, you decide to take a peek at your node_modules
-          folder and HOLY CRAP. It's FILLED with a bunch of sub folders! "is-even"? Did I even
-          install that? Do I really need "is-number"? Dependency Visualizer is a website that
-          lets you recursively view a graph of every dependency a package relies on. It also
+          folder and HOLY CRAP. It's FILLED with a bunch of sub folders! "is-even"? Did you even
+          install that? Do you really need "is-number"? Dependency Visualizer is a website that
+          lets you view a graph of every dependency a package relies on. It also
           shows you the stats of said package (assuming they're available). Did you know
           that "is-number" has been downloaded 126,000,000 times? And to think I barely passed
-          calculus...`
+          my math class...`
       },
       {
         heading: 'How it works',
         text: `
-          This project makes heavy use of JavaScript promises, and the npm registry API. When
-          you search for a package, the registry API is queried, and a list of that package's
+          This project makes heavy use of JavaScript promises and the npm registry API. When
+          you search for a package, the registry API is queried and a list of that package's
           dependencies are returned. For every dependency that's returned, another query is made
           until there are no more dependencies left to search. To prevent the search from taking
           forever, queries are made asynchronously and added to a stack of promises. Think of
           it like washing dirty dishes. When you finish washing a dirty dish, you take it off the
           unclean stack and put it on another stack to dry. When there are no more dirty dishes,
           what you have left is a stack of clean dishes. Using a graphing library, each dependency
-          is saved as parent node a children nodes. This allows every dependency to be rendered,
+          is saved as parent node and child nodes. This allows every dependency to be rendered
           with lines pointing to what packages rely on each other.`
       },
       {
@@ -178,7 +178,7 @@ const projectData = [
         text: `
           That data structures class I took in college finally payed off. I had to think of a fast
           and efficient way of querying data and displaying data. I needed to be able to make
-          hundreds of requests without waiting for each individual request to complete before starting 
+          hundreds of requests without waiting for each individual request to complete before starting
           the next one. I also needed a way to visualize the data. This led me to using stacks and queues
           for network requests and a graph to display dependencies. Another problem I faced was making
           sure I didn't keep making the same request. To solve this, I implemented my own cache using
@@ -202,7 +202,7 @@ const projectData = [
         text: `
           So, the screenshot might be kind of misleading. It's an earlier version
           of this site. Generally, what I like to do is redesign the site every once
-          a while to keep me on my toes and help me learn about design an other things.
+          a while to keep me on my toes and help me learn about design and other things.
           This website started back in 2017 as a single page glued together with some
           jQuery and Bootstrap sprinkled in. To be honest, I had no idea what I was doing...
           (I still don't). In early 2018, I decided to ditch jQuery for React and
@@ -210,27 +210,26 @@ const projectData = [
           it got the job done. When 2019 hit, I wanted to learn Vue so I completely
           redesigned the site, ditched Bootstrap, and decided to go with vanilla CSS/SCSS
           for everything (that's the screenshot you see above). Today, I'm still using Vue,
-          but I continue too style the site without the use of design or CSS libraries.
-          I think it gives me a much better appreciation for CSS and gets those
-          creative juices flowing.`
+          but I continue to style the site from scratch. I think it gives me a much better
+          appreciation for CSS and gets those creative juices flowing.`
       },
       {
         heading: 'The approach',
         text: `
-          When I get an idea for a site, one of the first things I do is look to 
+          When I get an idea for a project, one of the first things I do is look to
           others for design inspiration. Whenever when I see a website that looks nice,
-          I bookmark it, pop open my text editor, create a simple HTML and CSS file, and try to 
-          redesign parts of it from scratch. I always start simple, try to recreate a 
+          I bookmark it, pop open my text editor, create a simple HTML and CSS file, and try to
+          redesign parts of it from scratch. I always start simple, try to recreate a
           cool button hover effect or even something as simple as a shaped element.
           When I get stuck, I open up Chrome's dev tools and checkout the site's styling
           to see how they did it. Doing this helps me see that there's never just one way to
-          do anything and I get to learn how other developer think in the process. It really
+          do anything and I get to learn how other developers think in the process. It
           makes me realize just how much I don't know.`
       },
       {
         heading: 'How I build a project',
         text: `
-          Ok, I swear I do more than just make websites... Whenever I have an idea for a project,
+          Ok, I swear I do more than just make websites... Whenever I have an idea,
           (as idiotic as it may be), I jot it down and save it to a list of future projects.
           The idea here is that there's no such thing as a bad idea. The way I see it, if you
           learned something in the process, then that project has value. So, when I decide on
@@ -252,13 +251,13 @@ const projectData = [
         heading: 'What I learned',
         text: `
           There are no bad ideas, projects are important, and writing CSS is fun. Seriously.
-          I used to shy away from CSS because I didn't want to look up "how to center a div" for
-          the 1,000th time. After taking a bit of time to understand what CSS can do, it
-          actually piqued my interest. Projects are important too. It's not about showcasing
-          your skills or proving you can write Facebook in 10 lines of code, it's about what
-          you learn in the process. Making projects, I've learned how to design from scratch,
-          how to work with other developers, how to create REST APIs, how to deploy and host
-          applications, how to debug, how to work with databases, I mean, the list is endless.
+          I used to shy away from CSS because it seemed super complex. After taking a bit of
+          time to understand what CSS could do, it actually piqued my interest. Projects are
+          important too. It's not about showcasing your skills or proving you can write Facebook
+          in 10 lines of code, it's about what you learn in the process. Making projects, I've
+          learned how to design from scratch, how to work with other developers, how to create
+          REST APIs, how to deploy and host applications, how to debug, how to work with databases,
+          I mean, the list is endless.
         `
       }
     ]
