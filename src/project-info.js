df8cffbac8624be0433f015b5c1eda003bdac521
ctcuff <dev.ctcuff@gmail.com>
2021-02-03 12:56:57 -0500

    Added project

--- src/project-info.js
+++ src/project-info.js
@@ -1,39 +1,114 @@
 const projects = [
   {
+    title: 'Git Browser',
+    description: 'A new way to view GitHub repositories.',
+    backgroundImage: require('./assets/images/git-browser-fullscreen.png'),
+    foregroundImage: require('./assets/images/git-browser.png'),
+    imageAlt: 'Git Browser website screenshot'
+  },
+  {
     title: 'UCF Parking',
     description: 'A website and API to keep track of parking spaces.',
     backgroundImage: require('./assets/images/ucf-parking-fullscreen.png'),
     foregroundImage: require('./assets/images/ucf-parking.png'),
-    imageAlt: 'UCF parking website screenshot',
-    link: '/project?index=0'
-  },
-  {
-    title: 'Live Code',
-    description: 'A site that lets people code together.',
-    backgroundImage: require('./assets/images/livecode-fullscreen.png'),
-    foregroundImage: require('./assets/images/livecode.png'),
-    imageAlt: 'Live code website screenshot',
-    link: '/project?index=1'
+    imageAlt: 'UCF parking website screenshot'
   },
   {
     title: 'Dependency Visualizer',
     description: "View a graph of a JavaScript package's dependencies from npm.",
     backgroundImage: require('./assets/images/dependency-visualizer-fullscreen.png'),
     foregroundImage: require('./assets/images/dependency-visualizer.png'),
-    imageAlt: 'Dependency visualizer website screenshot',
-    link: '/project?index=2'
+    imageAlt: 'Dependency visualizer website screenshot'
   },
   {
     title: 'Portfolio',
     description: 'This very website and how I approach projects.',
     backgroundImage: require('./assets/images/portfolio-fullscreen.png'),
     foregroundImage: require('./assets/images/portfolio.png'),
-    imageAlt: 'Portfolio website screenshot',
-    link: '/project?index=3'
+    imageAlt: 'Portfolio website screenshot'
+  },
+  {
+    title: 'Live Code',
+    description: 'A site that lets people code together.',
+    backgroundImage: require('./assets/images/livecode-fullscreen.png'),
+    foregroundImage: require('./assets/images/livecode.png'),
+    imageAlt: 'Live code website screenshot'
   }
 ]
 
+projects.forEach((project, index) => (project.link = `/project?index=${index}`))
+
 const projectData = [
+  {
+    title: 'Git Browser',
+    image: {
+      src: require('./assets/images/git-browser.png'),
+      alt: 'Git Browser Website'
+    },
+    links: {
+      github: 'https://github.com/ctcuff/git-browser',
+      website: 'https://gitbrowser.io'
+    },
+    sections: [
+      {
+        heading: 'A new way to view code',
+        text: `
+          Have you ever been browsing through a GitHub repository, trying to look at
+          multiple files at once? Have you ever wanted to preview a 3D model in your
+          browser? Have you ever wanted to see what a video looks like without downloading it?
+          Maybe you wonder what that font would look like. Well, probably not, but if you did,
+          you're in luck! Git Browser takes all the code in a GitHub repository, and
+          puts it into a single page. No more jumping back and forth through your history to
+          look at those two files (you know the ones).`
+      },
+      {
+        heading: 'How it works',
+        text: `
+          It's simple: GitHub has a pretty will documented open API. This site simply takes
+          the link to a GitHub repository, does some fancy JavaScript "magic",
+          and displays each file in a folder-like structure. Unlike GitHub, files such as woff,
+          mp3, mp4, GLTF/GLB models, and a few others can all be previewed directly without having to
+          download and open them yourself. How does it work you probably didn't ask? Well, the GitHub API
+          returns the contents of a file in base64 format. This means that, in order to display the file,
+          we first have to decode it. Because a base64 string can be quite large, decoding can
+          not be done on the main thread because it blocks the UI, instead, a worker is used. Once the
+          file has been decoded, it's either displayed as a preview (like the astronaut model in the
+          screenshot), or as text in a read-only editor.`
+      },
+      {
+        heading: 'Unique challenges and solutions',
+        text: `
+          If I had a dollar for every bug I encountered while making this project, I wouldn't have
+          any student loan debt. As you know, GitHub returns the contents of a file in a base64 string.
+          "Ok, we can just decode and display it right? Easy!". Well, kinda? The first issue I faced was
+          that decoding a string causes some UTF characters to display incorrectly, like Japanese or Russian
+          characters. How do we fix this? The trick is to use JavaScript's Uint8Array and TextDecoder
+          to make sure each character of the base64 string gets properly decoded (huge shoutout to StackOverflow).
+          Then there's the problem of files without an extension. How do you know if a text file is a text
+          file if it's just named "arbitrary-file". Yet another hack, uh, I mean, "trick". Luckily, that decode
+          function we blatantly copied from StackOverflow throws an error if a base64 string cannot be properly decoded
+          (I swear this is a feature NOT a bug). If it throws an error, we assume that the file most likely
+          contains some binary data that isn't human readable. In that case, it won't be displayed as text.`
+      },
+      {
+        heading: 'What I learned',
+        text: `
+          Don't waste time pre-optimizing, it's a trap (sometimes). This was definitely one of the larger
+          projects I've made, and because of this, I spent more time fixing bugs and writing tests
+          than I did making stuff run faster. That being said, small changes here and there can result in
+          massive improvements. While testing large files, I found that a CSV with over 100,000 lines took
+          nearly 10 seconds to load. After a few optimizations, the load time fell down to around
+          800 milliseconds. This project also taught me how powerful MIME types are. Without any
+          external libraries, you can take a base64 string, add a MIME type of video/mp4, slap that
+          into a <video> tag, and boom, instant video preview. Another takeaway from this project: code
+          organization. This project uses React so I had to think about
+          how I wanted to break down each component. Maybe this input should be a search component,
+          or maybe that collapse element could be it's own component. By making my code re-usable and
+          modular,it becomes easier to test and modify in the future.
+          `
+      }
+    ]
+  },
   {
     title: 'UCF Parking',
     image: {
@@ -72,7 +147,7 @@ const projectData = [
       {
         heading: 'What I learned',
         text: `
-          Some of the biggest things I learned from this project are HTTP response codes,
+          Some of the biggest things I learned from this project were HTTP response codes,
           search queries, and deployment. Sure, I could just return some data or an error
           message, but it's important to know the difference between a 200 and a 418 status
           code. Then there's the query. I had to learn how to ask my API for specific data
@@ -82,57 +157,6 @@ const projectData = [
           project's memory usage and response time because of it. They even offer continuous
           integration and deployment, so pushing the next version of my site is as easy as
           pushing to a branch on GitHub.`
-      }
-    ]
-  },
-  {
-    title: 'Live Code',
-    image: {
-      src: require('./assets/images/livecode.png'),
-      alt: 'Live code website'
-    },
-    links: {
-      github: 'https://github.com/ctcuff/LiveCode',
-      website: 'https://ctcuff.github.io/LiveCode/'
-    },
-    sections: [
-      {
-        heading: 'Write code together',
-        text: `
-        Imagine this: you've got your coding interview scheduled. It's online. You're
-        excited because you finally get to showcase your coding skills. The time comes.
-        You get the link. You open up the website... and you're asked to write
-        code on Google Docs... yikes... Wouldn't it be nice to write code in your browser,
-        like you do with VSCode? Well fear not! LiveCode is a website that lets you write
-        code with other people. Just sign in, connect to a workspace, and code like you
-        never even left your text editor.`
-      },
-      {
-        heading: 'How it works',
-        text: `
-          Sockets! After signing in, you'll be able to connect to another
-          users' workspace or have others connect to yours. It works by broadcasting changes made
-          to your online editor to other users connected to your workspace using a library called
-          socket.io. A tiny NodeJS server handles socket events to ensure content is only broadcast
-          to users in the proper room. Google's Firebase is used to determine when users are online,
-          offline, or when a user gains and looses internet connection. To avoid broadcasting the
-          entire content of the editor on every keystroke, all the text in the editor is only
-          broadcast when a user first joins. Subsequent broadcasts contain only new characters
-          inserted and the line number/position of those characters. This equates to a better user
-          experience and less data transferred.`
-      },
-      {
-        heading: 'What I learned',
-        text: `
-          In JavaScript, there's a library for everything. One of the biggest things I
-          learned from this project was not to re-invent the wheel. There's a time when re-inventing
-          existing solutions is a good idea (you learn stuff in the process), but implementing user
-          authentication from scratch is not one of those times. Integrating Google's Firebase SDK,
-          I learned how to authenticate users with their Github and Google account, and I learned
-          how to implement Firebase security rules, preventing outside users from accessing
-          information they shouldn't have access to. Because a large part of the project was front
-          end, I learned a ton about material components, color, and responsive design. Yes,
-          I did have to Google "How to center a div" a few times... cut me some slack ok...`
       }
     ]
   },
@@ -259,6 +283,57 @@ const projectData = [
           REST APIs, how to deploy and host applications, how to debug, how to work with databases,
           I mean, the list is endless.
         `
+      }
+    ]
+  },
+  {
+    title: 'Live Code',
+    image: {
+      src: require('./assets/images/livecode.png'),
+      alt: 'Live code website'
+    },
+    links: {
+      github: 'https://github.com/ctcuff/LiveCode',
+      website: 'https://ctcuff.github.io/LiveCode/'
+    },
+    sections: [
+      {
+        heading: 'Write code together',
+        text: `
+        Imagine this: you've got your coding interview scheduled. It's online. You're
+        excited because you finally get to showcase your coding skills. The time comes.
+        You get the link. You open up the website... and you're asked to write
+        code on Google Docs... yikes... Wouldn't it be nice to write code in your browser,
+        like you do with VSCode? Well fear not! LiveCode is a website that lets you write
+        code with other people. Just sign in, connect to a workspace, and code like you
+        never even left your text editor.`
+      },
+      {
+        heading: 'How it works',
+        text: `
+          Sockets! After signing in, you'll be able to connect to another
+          users' workspace or have others connect to yours. It works by broadcasting changes made
+          to your online editor to other users connected to your workspace using a library called
+          socket.io. A tiny NodeJS server handles socket events to ensure content is only broadcast
+          to users in the proper room. Google's Firebase is used to determine when users are online,
+          offline, or when a user gains and looses internet connection. To avoid broadcasting the
+          entire content of the editor on every keystroke, all the text in the editor is only
+          broadcast when a user first joins. Subsequent broadcasts contain only new characters
+          inserted and the line number/position of those characters. This equates to a better user
+          experience and less data transferred.`
+      },
+      {
+        heading: 'What I learned',
+        text: `
+          In JavaScript, there's a library for everything. One of the biggest things I
+          learned from this project was not to re-invent the wheel. There's a time when re-inventing
+          existing solutions is a good idea (you learn stuff in the process), but implementing user
+          authentication from scratch is not one of those times. Integrating Google's Firebase SDK,
+          I learned how to authenticate users with their Github and Google account, and I learned
+          how to implement Firebase security rules, preventing outside users from accessing
+          information they shouldn't have access to. Because a large part of the project was front
+          end, I learned a ton about material components, color, and responsive design. Yes,
+          I did have to Google "How to center a div" a few times... cut me some slack ok...`
       }
     ]
   }
